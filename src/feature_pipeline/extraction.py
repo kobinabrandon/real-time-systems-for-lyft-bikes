064706dae752200a1b556fd4d409a62e23bcbf57
Kobina Brandon <kobinabrandon@proton.me>
2025-01-10 16:06:09 +0000

    The geodata from the station_information feed is now being saved properly. Looking at addign more custom types

--- src/feature_pipeline/extraction.py
+++ src/feature_pipeline/extraction.py
@@ -1,27 +1,27 @@
 import json
 from tqdm import tqdm
 from pathlib import Path
-from loguru import logger 
 
 from src.setup.custom_types import FeedData 
-from src.setup.paths import GEOGRAPHICAL_DATA, make_data_directories
 from src.feature_pipeline.geocoding import reverse_geocode
+from src.setup.paths import GEOGRAPHICAL_DATA, make_data_directories
 
 
 class GeodataExtractor:
     def __init__(self, city_name: str, feed_name: str, feed_data: FeedData | None = None) -> None:
         """
+        Extracts geodata from a specific feed from a particular city.  
 
         Args:
-            city_name: 
-            feed_name: 
+            city_name: the name of the city whose data we are sourcing 
+            feed_name: the name of the feed which we will be producing data 
             feed_data: data that has been fetched from the ("station_information") feed. 
         """
         self.city_name: str = city_name.lower()
         self.feed_name: str = feed_name.lower()
         self.feed_data: FeedData | None = feed_data 
-        self.path_to_official_geodata: Path = GEOGRAPHICAL_DATA / self.city_name / "official_station_geodata.json" 
         self.path_to_other_geodata: Path = GEOGRAPHICAL_DATA / self.city_name / f"{feed_name}_geodata.json" 
+        self.path_to_official_geodata: Path = GEOGRAPHICAL_DATA / self.city_name / "official_station_geodata.json" 
 
         make_data_directories()
 
@@ -35,10 +35,11 @@ class GeodataExtractor:
             all_station_data = self.feed_data["data"]["stations"]
             fetched_official_station_geodata = self.extract_official_station_geodata(items=all_station_data)  
             saved_official_station_geodata = self.get_saved_data(official=True)
-            saved_official_station_geodata.update(fetched_official_station_geodata)            
             
-            breakpoint()
-
+            if len(saved_official_station_geodata) > 0:
+                saved_official_station_geodata.append(fetched_official_station_geodata)
+            else:
+                saved_official_station_geodata.extend(fetched_official_station_geodata)
 
             self.save_data(data=saved_official_station_geodata, official=True)
         
@@ -84,7 +85,7 @@ class GeodataExtractor:
 
         return all_found_geodata 
 
-    def extract_official_station_geodata(self, items: list[dict[str, int | str]]) -> dict[str, str | list[float]]:
+    def extract_official_station_geodata(self, items: list[dict[str, int | str]]) -> list[dict[str, str | list[float]]]:
         """
         Goes through each element of the geodata from the "station_information" feed, and extracts the name, address, and 
         coordinate of each entry.
@@ -96,7 +97,7 @@ class GeodataExtractor:
            dict[str, str | list[float]]: all of the geodata that was made available by the "station_information" feed. 
         """
         assert self.feed_name == "station_information"
-        all_official_station_geodata: dict[str, str | list[float]] = {}
+        all_official_station_geodata: list[dict[str, str | list[float]]] = []
 
         for item in items:
             extracted_geodata = {
@@ -105,9 +106,7 @@ class GeodataExtractor:
                 "coordinate": [float(item["lat"]), float(item["lon"])] 
             }
 
-            all_official_station_geodata |= extracted_geodata
-           
-            breakpoint()
+            all_official_station_geodata.append(extracted_geodata) 
 
         return all_official_station_geodata
 
@@ -141,7 +140,7 @@ class GeodataExtractor:
 
         return False if coordinate not in station_info.values() else True
 
-    def save_data(self, data: dict[str, list[float]] | dict[str, str | list[float]], official: bool) -> None:
+    def save_data(self, data: list[dict[str, list[float]]] | list[dict[str, str | list[float]]], official: bool) -> None:
         """
         Save the station geodata that has been created or updated as a json file.
 
@@ -154,7 +153,7 @@ class GeodataExtractor:
         with open(path, mode="w") as file:
             json.dump(data, file)
 
-    def get_saved_data(self, official: bool) -> dict[str, list[float]] | dict[str, str | list[float]]:
+    def get_saved_data(self, official: bool) -> list[dict[str, list[float]]] | list[dict[str, str | list[float]]]:
         """
         Retrieve saved geodata as a dictionary 
 
@@ -163,12 +162,13 @@ class GeodataExtractor:
                       (which I deem to be the "official" source) or not. 
 
         Returns:
-            dict[str, list[float]]: the geodata that has been loaded. It may also be empty if there is no saved data.
+            list[dict[str, str | list[float]]]: the geodata that has been loaded. It may also be empty if 
+                                                there is no saved data.
         """
         path = self.path_to_official_geodata if official else self.path_to_other_geodata 
         if Path(path).exists():
             with open(path, mode="rb") as file:
                 return json.load(file)
         else:
-            return {}
+            return [] 
 
